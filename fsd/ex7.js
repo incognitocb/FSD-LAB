3a54e8179fcdef60216699f501b933d001d0772f
isaacgera <isaacgera2003@gmail.com>
2023-08-11 07:41:41 +0530

    ex7 update

--- fsd/ex7.js
+++ fsd/ex7.js
@@ -3,52 +3,47 @@
 //PugServer.js
 
 
-// Code express application using template-engine (pug)
 const express = require('express');
 const app = express();
 const port = 5000;
-// Set the view engine to Pug
-//app.set('views','./views');
- app.set('view engine', 'pug');
-// Middleware to parse request body
+
+app.set('view engine', 'pug');
 app.use(express.urlencoded({ extended: true }));
-// Function to check if a number is a palindrome
+
 function isPalindrome(number) {
- const reversedNumber = number.split('').reverse().join('');
- return number == reversedNumber;
+  const reversedNumber = number.split('').reverse().join('');
+  return number === reversedNumber; // Use strict equality here
 }
-// Render the initial form
+
 app.get('/', (req, res) => {
- res.render('index');
-});
-// Handle POST request to check palindrome
+  res.render('index');
+}
+
 app.post('/', (req, res) => {
-const number = req.body.number;
- const palindrome = isPalindrome(number);
- const message = {
- text: palindrome ? 'The number is a palindrome!' : 'The number is 
-not a palindrome.',
- class: palindrome ? 'success' : 'error'
- };
- res.render('index', { message });
-});
-// Start the server
-app.listen(port, () => {
- console.log(`Server is running on http://localhost:${port}`);
+  const number = req.body.number;
+  const palindrome = isPalindrome(number);
+  const message = {
+    text: palindrome ? 'The number is a palindrome!' : 'The number is not a palindrome.',
+    class: palindrome ? 'success' : 'error'
+  };
+  res.render('index', { message });
 });
 
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 index.pug
 
 doctype html
 html
- head
- title Palindrome Checker
- body
- h1 Palindrome Checker:
- form(action="/" method="POST")
- label(for="number") Enter a number:
- input(type="text" name="number" id="number")
- button(type="submit") Check Palindrome
- if message
- p(class=message.class) #{message.text}
+  head
+    title Palindrome Checker
+  body
+    h1 Palindrome Checker:
+    form(action="/" method="POST")
+      label(for="number") Enter a number:
+      input(type="text" name="number" id="number")
+      button(type="submit") Check Palindrome
+    if message
+      p(class=message.class) #{message.text}
